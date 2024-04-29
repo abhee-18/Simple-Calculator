092d2287015924fad9edc50ef6f2c6c3cb18ddb3
abhee-18 <151569565+abhee-18@users.noreply.github.com>
2024-04-29 21:40:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const parentDOM = document.getElementById("display");
+function appendToDisplay(input) {
+    display.value += input;
+}
+function clearDisplay() {
+    display.value = "";
+}
+function calculate(){
+    display.value = eval(display.value)
+}
+function calculate(){
+    try{
+        display.value =eval(display.value)
+    }
+    catch(error){
+        display.value="Error"
+    }
+}
